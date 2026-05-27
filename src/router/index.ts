22f5ff5351c1066a3ffa0d22692265cce6797c7a
wwinz0 <bustamantediane.0106@gmail.com>
2026-05-27 22:19:26 +0800

    add ProfilePage component and update router configuration

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory, type RouteRecordRaw } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import GamePage from '../views/GamePage.vue'
 import ProfilePage from '@/views/ProfilePage.vue'
+import ProfilePage from '@/views/ProfilePage.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
