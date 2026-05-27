61ed7e14cdc8d3903734ddd3e4f02558aebc477d
raretutal <retutal.ryananthony@gmail.com>
2026-05-27 18:15:22 +0800

    changed temporary home path to GamePage

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,13 +1,14 @@
 import { createRouter, createWebHistory, type RouteRecordRaw } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import GamePage from '../views/GamePage.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: HomeView,
+      path: '/', // Change for home view
+      name: 'GamePage',
+      component: GamePage,
     },
     {
       path: '/about',
@@ -17,6 +18,11 @@ const router = createRouter({
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue'),
     },
+    {
+      path: '/GamePage',
+      name: 'GamePage',
+      component: GamePage
+    }
   ],
 })
 
