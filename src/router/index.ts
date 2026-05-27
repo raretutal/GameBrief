6c916b1e6ab6a6df625f1c1e0e965187444e1796
raretutal <retutal.ryananthony@gmail.com>
2026-05-27 18:10:06 +0800

    added shims-vue.d.ts file to interpret .vue files and their exports

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory, type RouteRecordRaw } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 
 const router = createRouter({
