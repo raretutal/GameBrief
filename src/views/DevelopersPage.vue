7844de35b4042c5f09489b52a1ff750280c28931
raretutal <retutal.ryananthony@gmail.com>
2026-05-28 18:08:09 +0800

    added DevelopersPage for dev info (just dev name, img, and url)

---
+++ src/views/DevelopersPage.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import NavBar from '../components/NavBar.vue'
+
+</script>
+
+<template>
+    <!-- Background div-->
+    <NavBar/>
+    <div class="min-h-screen w-full bg-[radial-gradient(circle_at_top,#3b3b3b_0%,#111_45%,#000_100%)]">
+
+
+    </div>
+
+
+
+</template>
+
