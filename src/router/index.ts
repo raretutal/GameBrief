da3b58d681bdb8d6b384071b88ad3d5a0b604367
raretutal <retutal.ryananthony@gmail.com>
2026-05-30 09:44:43 +0800

    fixed typo in router

--- src/router/index.ts
+++ src/router/index.ts
@@ -9,7 +9,7 @@ import SignUp from '@/views/SignUp.vue'
 import SignIn from '@/views/SignIn.vue'
 import LandingPage from '@/views/LandingPage.vue'
 import GameView from '@/views/GameView.vue'
-import UserProfile from '@views/UserProfile.vue'
+import UserProfile from '@/views/UserProfile.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
